a393090cf37c440e85fcf00a1c4899498b163876
Denis Bogdanov <bogdanov.math@gmail.com>
2018-01-17 23:44:51 +0300

    Add category repository.

--- src/main/java/ru/bogdanium/giflib/data/GifRepository.java
+++ src/main/java/ru/bogdanium/giflib/data/GifRepository.java
@@ -10,12 +10,12 @@ import java.util.List;
 @Component
 public class GifRepository {
     private static final List<Gif> ALL_GIFS = Arrays.asList(
-            new Gif("android-explosion", LocalDate.of(2018, 1, 17), "John Doe", false),
-            new Gif("ben-and-mike", LocalDate.of(2018, 1, 17), "Jane Doe", false),
-            new Gif("book-dominos", LocalDate.of(2018, 1, 17), "John Doe", true),
-            new Gif("compiler-bot", LocalDate.of(2018, 1, 17), "Jane Doe", false),
-            new Gif("cowboy-coder", LocalDate.of(2018, 1, 17), "John Doe", false),
-            new Gif("infinite-andrew", LocalDate.of(2018, 1, 17), "Jane Doe", true)
+            new Gif("android-explosion", 1, LocalDate.of(2018, 1, 17), "John Doe", false),
+            new Gif("ben-and-mike", 2, LocalDate.of(2018, 1, 17), "Jane Doe", false),
+            new Gif("book-dominos", 3, LocalDate.of(2018, 1, 17), "John Doe", true),
+            new Gif("compiler-bot", 3, LocalDate.of(2018, 1, 17), "Jane Doe", false),
+            new Gif("cowboy-coder", 1, LocalDate.of(2018, 1, 17), "John Doe", false),
+            new Gif("infinite-andrew", 2, LocalDate.of(2018, 1, 17), "Jane Doe", true)
     );
 
     public Gif findByName(String name) {
