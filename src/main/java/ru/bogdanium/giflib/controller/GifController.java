c7d27fc4c9ea45c97e2899560ec4532d333ce28a
Denis Bogdanov <bogdanov.math@gmail.com>
2018-01-17 22:48:12 +0300

    Transfer data to template.

--- src/main/java/ru/bogdanium/giflib/controller/GifController.java
+++ src/main/java/ru/bogdanium/giflib/controller/GifController.java
@@ -1,13 +1,25 @@
 package ru.bogdanium.giflib.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.bogdanium.giflib.model.Gif;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
 
-    @RequestMapping(value = "/")
+    @RequestMapping("/")
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2018, 1, 17),
+                "Denis", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
