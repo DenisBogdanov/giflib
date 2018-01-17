55fd344466314aa10eb46da8d578de31b77bd394
Denis Bogdanov <bogdanov.math@gmail.com>
2018-01-17 23:28:20 +0300

    Add dynamic detail page.

--- src/main/java/ru/bogdanium/giflib/controller/GifController.java
+++ src/main/java/ru/bogdanium/giflib/controller/GifController.java
@@ -3,6 +3,7 @@ package ru.bogdanium.giflib.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.bogdanium.giflib.data.GifRepository;
 import ru.bogdanium.giflib.model.Gif;
@@ -18,9 +19,9 @@ public class GifController {
         return "home";
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap) {
-        Gif gif = gifRepository.findByName("android-explosion");
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
